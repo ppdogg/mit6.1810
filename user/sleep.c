0dd160d7e080980d7560970b9adf71961e9f0a72
ppdogg <ppdoooog@gmail.com>
2024-02-26 21:46:27 +0800

    lab1-sleep

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+
+#include "kernel/stat.h"
+
+#include "kernel/fcntl.h"
+
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+  int sec;
+
+  if (argc <= 1) {
+    fprintf(2, "usage: sleep seconds\n");
+    exit(1);
+  }
+
+  sec = atoi(argv[1]);
+  sleep(sec);
+  exit(0);
+}
